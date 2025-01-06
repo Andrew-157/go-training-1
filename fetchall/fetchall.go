ac01e1daea4c5db91c347d6fdda72f0cbf2e9c2a
Andrew Subotin <subotinandrey5@gmail.com>
2025-01-06 12:46:05 +0100

    fetchall

---
+++ fetchall/fetchall.go
@@ -0,0 +1,48 @@
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+/*
+Example output:
+root@fedora:~/go_dir/fetchall# go run . http://google.com http://youtube.com http://amazon.com
+0.19s    19226  http://google.com
+0.56s   578936  http://youtube.com
+0.97s     6591  http://amazon.com
+0.97s elapsed
+*/
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v\n", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
---
+++ fetchall/go.mod
@@ -0,0 +1,3 @@
+module example.com/fetchall
+
+go 1.23.4
