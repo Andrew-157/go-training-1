acc632da8933d0e738a1477f337aa92876c1d87d
Andrew Subotin <subotinandrey5@gmail.com>
2025-01-06 12:17:06 +0100

    Added fetch2

---
+++ fetch2/fetch2.go
@@ -0,0 +1,32 @@
+// Fetch2 prints the content found at a URL,
+// adds prefix 'http://' to the provided url, if it is missing
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const urlPrefix = "http://"
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, urlPrefix) {
+			url = urlPrefix + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
---
+++ fetch2/go.mod
@@ -0,0 +1,3 @@
+module example.com/fetch2
+
+go 1.23.4
