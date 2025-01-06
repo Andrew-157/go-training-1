1a06b5dcec7b2198d2f1bb533bf5e25b903bf492
Andrew Subotin <subotinandrey5@gmail.com>
2025-01-06 13:53:29 +0100

    Started writing fetchcaching

---
+++ fetchcaching/fetchcaching.go
@@ -0,0 +1,80 @@
+// fetchcaching - fetches each url twice to compare the time it took
+// to fetch each url for the second time
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	httpUrlPrefix  = "http://"
+	httpsUrlPrefix = "https://"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetchCache(url, ch, 2)
+	}
+	for range len(os.Args[1:]) {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("Total: %.2fs elapsed.\n", time.Since(start).Seconds())
+}
+
+func fetchCache(url string, ch chan<- string, tries int) {
+	var start time.Time
+	tryTimeElapsed := make(map[int]float64)
+	tryBytesRetrieved := make(map[int]int64)
+	var triesSlice []int
+	for i := 1; i <= tries; i++ {
+		// fetch url `tries` times in succession and write time it took for each try
+		triesSlice = append(triesSlice, i)
+		start = time.Now()
+		resp, err := http.Get(url)
+		if err != nil {
+			ch <- fmt.Sprintf("fetchcaching: %d try fetching url %s: %v", i, url, err)
+			return
+		}
+		tryTimeElapsed[i] = time.Since(start).Seconds()
+		var (
+			filename string
+			prefix   string
+		)
+		if strings.HasPrefix(url, httpUrlPrefix) {
+			prefix = httpUrlPrefix
+		} else if strings.HasPrefix(url, httpsUrlPrefix) {
+			prefix = httpsUrlPrefix
+		}
+		filename = fmt.Sprintf("%s-%d.html", strings.Join(strings.Split(url, prefix)[1:], ""), i)
+		f, err := os.Create(filename)
+		if err != nil {
+			ch <- fmt.Sprintf("fetchcaching: while fetching url %s and creating file: %s: %v", url, filename, err)
+		}
+		nbytes, err := io.Copy(f, resp.Body)
+		resp.Body.Close()
+		fileCloseError := f.Close()
+		if fileCloseError != nil {
+			ch <- fmt.Sprintf("while closing file: %s", filename)
+		}
+		if err != nil {
+			ch <- fmt.Sprintf("while reading %s: %v\n", url, err)
+			return
+		}
+		tryBytesRetrieved[i] = nbytes
+	}
+	resultData := fmt.Sprintf("%s: ", url)
+	for index, try := range triesSlice {
+		resultData += fmt.Sprintf("%d try: [%.2fs elapsed|%7d bytes retrieved]", try, tryTimeElapsed[try], tryBytesRetrieved[try])
+		if index != len(triesSlice)-1 {
+			resultData += ", "
+		}
+	}
+	ch <- resultData
+}
---
+++ fetchcaching/go.mod
@@ -0,0 +1,3 @@
+module example.com/fetchcaching
+
+go 1.23.4
